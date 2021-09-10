38dd46212d8f4285bde3e35dcd033fe11d60e6fa
Jacob <jacobmines@protonmail.com>
2021-09-10 14:43:42 -0400

    Commit #6 - Restructured directories. Renamed several files.
    Commit #6 - Restructured directories. Renamed _variables.js, _functions.js & _objects.js.

--- _Front-End/JavaScript/_objects/_objects.js
+++ _Front-End/JavaScript/_objects/_objects.js
@@ -4,6 +4,8 @@
 
 // Objects must have a reference point, something to refer to them as. We do this by assigning them to a variable with a name we can later use to call on the object or any of it's key:value pairs, or a method().
 // Note that the functions created within an object can be a standard named function, or they could be an arrow function. A function inside of an object is called a METHOD.
+// ? Interesting note: Object key:value pairs are NOT stored in the order they are inserted when iterated through as an array. They are in fact alphabetized. Maps however DO retain their original or insertion/added order. Which is something to be aware of if that aspect is required.
+
 // The basic syntax to create an object is as follows:
 const tutorialObject = {
     key1: "value1",
@@ -233,97 +235,11 @@ const newUserProfile = {
 // the third value just demonstrates that we can add 'on the fly' a new key:value pair to the new object upon creation at the same time.
 // ? if we were to just leave it at this, then we would in fact be creating a new object named 'createdUser' with a mix of both the original 'userProfile' key:values as well as the collected key:value data from 'newUserProfile' (+ the new verified:true key:value set), BUT in doing so, we would be nesting the user and newUserProfile objects in this new object seeing as they're already objects.
 // * to circumvent this, we use the spread operator (...) IN FRONT of the objects being injected.
-const createdUser = { ...userProfile,... newUserProfile, verified: true}
+const createdUser = {
+    ...userProfile,
+    ...newUserProfile,
+    verified: true
+}
 console.log(createdUser)
 // and we can see this works perfectly on view of the console.log()
 // * note: the key:value pairs that are being 'overwritten/mixed' for lack of a better term between 'userProfile' and 'newUserProfile' must be identical in order to 'overwrite' the 'blank' or 'old' key:value pairs, otherwise new key:values will just be created alongside the other key:value pairs, as one would expect.
-
-
-
-
-
-
-
-
-// TODO: Classes - Templates/Blueprints for Objects
-// a class is a template or blueprint for how you want an object constructed.
-// after writing the keyword 'class' followed by the desired className, we use the 'constructor' method.
-// the basic syntax is:
-// class className {
-//     constructor(propertyName1, propertyName2) {
-//
-//     }
-// }
-
-// we'll create a new class called 'Car' that takes 4 properties: make, model, year & cost.
-// this will now be the template/blueprint for all new objects we make using the 'Car' class template.
-// an actual example:
-class Car {
-    constructor(make, model, year, cost) {
-        this.make = make
-        this.model = model
-        this.year = year
-        this.cost = cost
-    }
-}
-
-// TODO: To USE the Class
-// we use the 'new' keyword with the the name of the class followed by the params in brackets.
-// make sure to follow traditional data type rules (quotes for strings, etc.)
-// let newObjName = new className(param1, param2, param3, etc)
-
-let firstCar = new Car("Ford", "F150", 2022, 74999)
-console.log(firstCar)
-
-// we can of course also create methods within the class template
-class User {
-    constructor(firstName, lastName, age, occupation, income) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.age = age
-        this.occupation = occupation
-        this.income = income
-    }
-    birthYear() {
-        return 2021 - this.age
-    }
-}
-
-let JRose = new User("Jenny Rose", "Olais", 18, "Student", 7850)
-let message = `${JRose.firstName} ${JRose.lastName} is an ${JRose.age} year old ${JRose.occupation} that makes $${JRose.income} per year.`
-console.log(message)
-
-
-// TODO: Class Inheritance or 'Extending'
-// we are able to 'inherit' or 'pull' CLASS properties and methods from an existing class and bring them into a new class.
-// it's a slightly more complicated concept to understand but easy once you get it.
-// to create a class inheritance we use the 'extend' keyword.
-// we'll 'extend' or 'pull' (as I prefer to think of it) some properties/methods from the class 'User' above.
-// we start with the class keyword followed by the new class name and the extend keyword followed again by the class we're 'pulling from'.
-class AdditionalStats extends User {
-    // the arguments within the constructor need to have ALL the params that will be used or inherited from the reference class.
-    // below we're inheriting the params: firstName, lastName, age, occupation and income from the class template above called 'User'.
-    constructor(firstName, lastName, age, occupation, income, gender, pronouns, sexualPreference, city, country) {
-        // we use the 'super' function (with the desired arguments passed) to reference/pull from the class we're inheriting or extending from.
-        super(firstName, lastName, age, occupation, income)
-        // as gender, pronouns, sexualPreference, city & country are new properties, we have to declare them here as we normally would.
-        this.gender = gender
-        this.pronouns = pronouns
-        this.sexualPreference = sexualPreference
-        this.city = city
-        this.country = country
-    }
-    // we've also created a new method called 'summary()' which console.logs our message.
-    summary() {
-        console.log(`${this.firstName} ${this.lastName} is a ${this.age} year old ${this.gender} from ${this.city}, ${this.country}, who works as a ${this.occupation} making ${this.income} a year. This person uses the pronouns "${this.pronouns}" and prefers ${this.sexualPreference} sexually.`)
-    }
-}
-
-// here we use the new CLASS "AdditionalStats" that is inheriting various properties from the CLASS "User" to create the new OBJECT called "Ares".
-let Ares = new AdditionalStats("Ares", "Olais", 28, "Saleswoman", 10500, "Female", "She/Her", "Men", "Cebu", "Philippines")
-
-// this will display the object and it's properties and methods.
-console.log(Ares)
-
-// this calls the 'summary()' method within the object "Ares".
-Ares.summary()
