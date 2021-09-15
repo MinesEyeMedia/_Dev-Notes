2acb91909a0d3bec2dbcf24deba5381f1e0748b3
Jacob <jacobmines@protonmail.com>
2021-09-14 22:12:07 -0400

    Commit #35 - Created & wrote async.html, promises.js & asynchronous.js.
    Commit #35 - Created & wrote _async.html, _promises.js & _asynchronous.js.

--- README.md
+++ README.md
@@ -35,3 +35,4 @@ A place to store all development related notes, from front-end to back-end to de
 - Commit #32 - Created & wrote _http-status-codes.md.
 - Commit #33 - Created & wrote _fetch.js & _fetch.html.
 - Commit #34 - Created & wrote _callbacks.js page.
+- Commit #35 - Created & wrote _async.html, _promises.js & _asynchronous.js.
---
+++ _Front-End/JavaScript/_async-javascript/_asynchronous.js
@@ -0,0 +1,11 @@
+// TODO: ================================ //
+// TODO: JavaScript Asynchronous          //
+// TODO: ================================ //
+
+console.log(`======================================`)
+console.log(`== JavaScript Asynchronous          ==`)
+console.log(`======================================`)
+
+// * In the 'Callbacks.js' page, the examples we used were very basic for demonstration/understanding purposes.
+
+// * Where callbacks really shine are in asynchronous functions, where one function has to wait for another function (like waiting for a file to load).
